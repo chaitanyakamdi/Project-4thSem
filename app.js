371fec65700785e1503ad46f9fe1b60cd2887a6a
Khushal Paunkar <khushalpaunkar79@gmail.com>
2026-02-20 21:24:24 +0530

    initial commit on paunkar branch

---
+++ app.js
@@ -0,0 +1,5 @@
+document.getElementById("signupForm")?.addEventListener("submit", function(e){
+  e.preventDefault();
+  alert("Account Created Successfully!");
+  window.location.href = "index.html";
+});
